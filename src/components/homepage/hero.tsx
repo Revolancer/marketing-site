3c29a06ac96a3564fa250cea12bc5daa9fbe9918
Skye Brady <skye@revolancer.com>
2023-06-12 08:58:29 +0000

    hero area

--- src/components/homepage/hero.tsx
+++ src/components/homepage/hero.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import { ColumnLayout, FullWidth } from "../layout/columns";
 import { Div } from "../layout/div";
+import { Flex } from "../layout/flex";
+import { H1, H4 } from "public/text/headings";
+import { P, SubtitleOne } from "public/text/text";
+import { Button } from "../navigation/button";
 
 export const Hero = () => {
   return (
@@ -32,7 +36,7 @@ export const Hero = () => {
         />
         <Div
           css={{
-            maxHeight: "564px",
+            maxHeight: "1024px",
             maxWidth: "1440px",
             minWidth: "360px",
             minHeight: "564px",
@@ -50,11 +54,44 @@ export const Hero = () => {
               position: "absolute",
               top: 0,
               left: 0,
+              height: "100%",
+              width: "100%",
             }}
             quality={100}
           ></Image>
           <ColumnLayout>
-            <FullWidth>Hero</FullWidth>
+            <FullWidth>
+              <Flex
+                css={{
+                  color: "$white",
+                  flexDirection: "column",
+                  paddingBlock: "16px",
+                  "@md": { flexDirection: "row", alignItems: "end" },
+                }}
+              >
+                <Flex column>
+                  <H1>Exchange services with other online professionals.</H1>
+                  <H4>
+                    Exchange services, earn credits, then spend them on what you
+                    need to grow your online business.
+                  </H4>
+                  <Button href="https://app.revolancer.com/register">
+                    Join Now
+                  </Button>
+                </Flex>
+                <Image
+                  src="/homepage/hero-image.png"
+                  width={533}
+                  height={400}
+                  alt=""
+                  style={{
+                    flexShrink: 0,
+                    minWidth: "400px",
+                    objectFit: "scale-down",
+                  }}
+                />
+              </Flex>
+            </FullWidth>
           </ColumnLayout>
         </Div>
       </Div>
