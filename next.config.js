d05570dae2d9518d0e16bd979bbf7b9c4b27211f
Skye Brady <skye@revolancer.com>
2023-06-13 15:24:39 +0000

    disable trailing slash

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
   rewrites: async () => {
     return {
       beforeFiles: [
         {
           source: "/magazine",
-          destination: "https://iris.rvinternals.com/",
+          destination: "https://iris.rvinternals.com",
         },
         {
           source: "/magazine/:path*",
-          destination: "https://iris.rvinternals.com/:path*/",
+          destination: "https://iris.rvinternals.com/:path*",
         },
       ],
     };
