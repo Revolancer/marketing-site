0a8456ba73b983ce928e591837cbf0e2899adce9
Skye Brady <skye@revolancer.com>
2023-06-13 14:19:51 +0000

    Add google analytics

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,6 +2,7 @@ import { Header } from "./header";
 import { Footer } from "../foot/footer";
 import { styled } from "@/stitches.config";
 import Head from "next/head";
+import Script from "next/script";
 
 const Container = styled("div", {
   minHeight: "100vh",
@@ -18,6 +19,19 @@ export const PrimaryLayout = ({ children }: { children?: any }) => {
           professionals.
         </meta>
       </Head>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-TRKYDYVYSJ"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-TRKYDYVYSJ');
+        `}
+      </Script>
       <Container>
         <Header />
         <main>{children}</main>
