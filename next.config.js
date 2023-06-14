1a02c702d4cec700fce3b8561ec0b7eaf8683f24
Skye Brady <skye@revolancer.com>
2023-06-14 14:04:25 +0100

    ready to deploy

--- next.config.js
+++ next.config.js
@@ -1,19 +1,44 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
-  rewrites: async () => {
-    return {
-      beforeFiles: [
-        {
-          source: "/magazine/",
-          destination: "https://iris.rvinternals.com/",
-        },
-        {
-          source: "/magazine/:path*",
-          destination: "https://iris.rvinternals.com/:path*/",
-        },
-      ],
-    };
+  async redirects() {
+    return [
+      {
+        source: "/find-freelancers/",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/find-clients/",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/plus/",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/about/",
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/how-it-works/",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/terms-and-conditions/",
+        destination: "/terms/",
+        permanent: true,
+      },
+      {
+        source: "/privacy-policy/",
+        destination: "/privacy/",
+        permanent: true,
+      },
+    ];
   },
 };
 
