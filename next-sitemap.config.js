13d77d7cd090e3f1dbb7f8b43a3f1859ec60d972
Skye Brady <skye@revolancer.com>
2023-07-03 15:53:56 +0000

    Add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://revolancer.com",
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    additionalSitemaps: ["https://revolancer.com/magazine/sitemap_index.xml"],
+  },
+};
